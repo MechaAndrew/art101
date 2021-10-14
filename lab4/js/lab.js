9290608ef7911bacc3c6f0b586012b3e9f97eca3
MechaAndrew <mechaandrewdk.com@gmail.com>
2021-10-14 00:27:27 -0700

    lab4

---
+++ lab4/js/lab.js
@@ -0,0 +1,22 @@
+/**
+* Summary.
+* Description.
+* @link URL
+* @file this file defines the myclass class
+* @author Sophia Syed & Andrew Dresel-Kurtz.
+* @since 10.12.21
+*/
+
+/*
+Enter the amount of people playing (2+)
+The computer randomly generates a word that is stored in a variable
+Print a blank line for each letter in the word to represent the word length
+The player types in either a letter or whole word
+  If; that letter is in the picked word, then add a point to the number of successes
+  Else; add a point to how many fails the user can do and draw the next body part of the hangman. Along with making the next player guess (if there is one)
+    If; the number of fails is >= 9 the user has lost
+    Display failure message
+    Else if; the number of successes is >= length of word the player wins
+    Display success message
+Repeat Process
+*/
