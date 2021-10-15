9bc8b93261b6b37b736b18b76b9c79208e583c39
MechaAndrew <mechaandrewdk.com@gmail.com>
2021-10-15 14:09:02 -0700

    labedit

--- lab5/js/lab5.js
+++ lab5/js/lab5.js
@@ -24,4 +24,4 @@ document.writeln("Make: " + make + "<br>");
 document.writeln("Model: " + model + "<br>");
 document.writeln("Color: " + color + "<br>");
 document.writeln("Year: " + year + "<br>");
-document.writeln("Age: " + age " years<br>");
+document.writeln("Age: " + age + " years<br>");
