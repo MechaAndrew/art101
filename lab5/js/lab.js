ab3b47e096988094769b5dc769d70e1bf93770d3
MechaAndrew <mechaandrewdk.com@gmail.com>
2021-10-15 13:34:14 -0700

    lab 5

---
+++ lab5/js/lab.js
@@ -0,0 +1,27 @@
+/**
+* Summary.
+* Description.
+* @link URL
+* @file this file defines the myclass class
+* @author Andrew Dresel-Kurtz.
+* @since 10.15.21
+*/
+
+//Declare and Define Variables
+var make, model, color, year, age, ownIt;
+
+make = "Toyota Rav4";
+model = "2019 Toyota RAV4 Excel (AXAH52)";
+color = "Red";
+year = 2019;
+ownIt = true;
+
+//calculate
+age = 2021 - year;
+
+//output
+document.writeln("Make: " + make + "<br>");
+document.writeln("Model: " + model + "<br>");
+document.writeln("Color: " + color + "<br>");
+document.writeln("Year: " + year + "<br>");
+document.writeln("Age: " + age " years<br>");
