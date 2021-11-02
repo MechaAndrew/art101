c0f8de3e7fe792991a893b9bb61ef269f2d2fdeb
MechaAndrew <mechaandrewdk.com@gmail.com>
2021-11-01 19:40:31 -0700

    lab 9

--- lab9/js/lab9.js
+++ lab9/js/lab9.js
@@ -7,13 +7,14 @@
 * @since 11.1.21
 */
 
-var outputEl = document.getElementById('output');
+var outputEl;
+outputEl = document.getElementById("output");
 
 var new1El = document.createElement("p");
-new1El.innerHTML = "<p>Something New</p>"
+new1El.innerHTML = "<p>Something New</p>";
 
 var new2El = document.createElement("p");
 new2El.innerHTML = "<p>Something New the sequelling</p>";
 
 outputEl.appendChild(new1El);
-outputEl.appendChild(new2El);
+//outputEl.appendChild(new2El);
