9e5e77805a902c2f6c9f7e91d65fead4d2774400
MechaAndrew <mechaandrewdk.com@gmail.com>
2021-11-01 19:09:35 -0700

    lab 9

---
+++ lab9/js/lab9.js
@@ -0,0 +1,19 @@
+/**
+* Summary.
+* Description.
+* @link URL
+* @file
+* @author Andrew Dresel-Kurtz.
+* @since 11.1.21
+*/
+
+var outputEl = document.getElementById('output');
+
+var new1El = document.createElement("p");
+new1El.innerHTML = "<p>Something New</p>"
+
+var new2El = document.createElement("p");
+new2El.innerHTML = "<p>Something New the sequelling</p>";
+
+outputEl.appendChild(new1El);
+outputEl.appendChild(new2El);
